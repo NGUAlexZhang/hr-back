0eb6ff4e39ffaf87c7401780447683677ac61041
NGUAlexZhang <2677374543@qq.com>
2021-10-26 12:37:30 +0800

    this is the first

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import moment  from 'moment'
+// eslint-disable-next-line no-unused-vars
+Vue.filter('moment', function (value, formatString) {
+  formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
+  return moment(value).format("YYYY-MM-DD"); // value可以是普通日期 20170723
+});
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
